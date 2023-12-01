18dcb11aee482488af77e9257eb997deb81e94a5
Keith A. Lewis <keithalewis@live.com>
2023-12-01 10:05:20 -0500

    sync

--- fms_duckdb.h
+++ fms_duckdb.h
@@ -11,6 +11,149 @@
 
 namespace fms::duckdb {
 
+// DUCKDB_TYPE_XXX, C type, description
+#define DUCKDB_TYPE_(X) \
+	X(BOOLEAN, bool, "") \
+	X(TINYINT, int8_t, "") \
+	X(SMALLINT, int16_t, "") \
+	X(INTEGER, int32_t, "") \
+	X(BIGINT, int64_t, "") \
+	X(UTINYINT, uint8_t, "") \
+	X(USMALLINT, uint16_t, "") \
+	X(UINTEGER, uint32_t, "") \
+	X(UBIGINT, uint64_t, "") \
+	X(FLOAT, float, "") \
+	X(DOUBLE, double, "") \
+	X(TIMESTAMP, duckdb_timestamp, " in microseconds") \
+	X(DATE, duckdb_date, "") \
+	X(TIME, duckdb_time, "") \
+	X(INTERVAL, duckdb_interval, "") \
+	X(HUGEINT, duckdb_hugeint, "") \
+	X(VARCHAR, const char*, "") \
+	X(BLOB, duckdb_blob, "") \
+	X(DECIMAL, decimal, "") \
+	X(TIMESTAMP_S, duckdb_timestamp, " in seconds") \
+	X(TIMESTAMP_MS, duckdb_timestamp, " in milliseconds") \
+	X(TIMESTAMP_NS, duckdb_timestamp, " in nanoseconds") \
+	X(UUID, duckdb_hugeint, "") \
+	X(ENUM, enum type, " only useful as logical type") \
+	X(BIT, duckdb_bit, "") \
+
+	//X(LIST, list type, " only useful as logical type") \
+	//X(STRUCT, struct type, " only useful as logical type") \
+	//X(MAP, map type, " only useful as logical type") \
+	//X(UNION, union type, " only useful as logical type") \
+
+	// https://duckdb.org/docs/archive/0.9.2/api/c/appender
+	class appender {
+		duckdb_appender app;
+	public:
+		appender(duckdb_connection con, const char* schema, const char* table)
+		{
+			duckdb_state state = duckdb_appender_create(con, schema, table, &app);
+			if (DuckDBSuccess != state) {
+				throw std::runtime_error(duckdb_appender_error(app));
+			}
+		}
+		// default schema
+		appender(duckdb_connection con, const char* table)
+			: appender(con, nullptr, table)
+		{ }
+		appender(const appender&) = delete;
+		appender& operator=(const appender&) = delete;
+		~appender()
+		{
+			duckdb_appender_destroy(&app);
+		}
+
+		// finalize appends
+		duckdb_state end_row() const
+		{
+			return duckdb_appender_end_row(app);
+		}
+
+		duckdb_state append(bool b) const
+		{
+			return duckdb_append_bool(app, b);
+		}
+		duckdb_state append(int8_t i) const
+		{
+			return duckdb_append_int8(app, i);
+		}
+		duckdb_state append(int16_t i) const
+		{
+			return duckdb_append_int16(app, i);
+		}
+		duckdb_state append(int32_t i) const
+		{
+			return duckdb_append_int32(app, i);
+		}
+		duckdb_state append(int64_t i) const
+		{
+			return duckdb_append_int64(app, i);
+		}
+		duckdb_state append(duckdb_hugeint i) const
+		{
+			return duckdb_append_hugeint(app, i);
+		}
+		duckdb_state append(uint8_t i) const
+		{
+			return duckdb_append_uint8(app, i);
+		}
+		duckdb_state append(uint16_t i) const
+		{
+			return duckdb_append_uint16(app, i);
+		}
+		duckdb_state append(uint32_t i) const
+		{
+			return duckdb_append_uint32(app, i);
+		}
+		duckdb_state append(uint64_t i) const
+		{
+			return duckdb_append_uint64(app, i);
+		}
+		duckdb_state append(float x) const
+		{
+			return duckdb_append_float(app, x);
+		}
+		duckdb_state append(double x) const
+		{
+			return duckdb_append_double(app, x);
+		}
+		duckdb_state append(duckdb_date d) const
+		{
+			return duckdb_append_date(app, d);
+		}
+		duckdb_state append(duckdb_time t) const
+		{
+			return duckdb_append_time(app, t);
+		}
+		duckdb_state append(duckdb_timestamp t) const
+		{
+			return duckdb_append_timestamp(app, t);
+		}
+		duckdb_state append(duckdb_interval t) const
+		{
+			return duckdb_append_interval(app, t);
+		}
+		duckdb_state append(const char* t) const
+		{
+			return duckdb_append_varchar(app, t);
+		}
+		duckdb_state append(const char* t, idx_t n) const
+		{
+			return duckdb_append_varchar_length(app, t, n);
+		}
+		duckdb_state append(const void* t, idx_t n) const
+		{
+			return duckdb_append_blob(app, t, n);
+		}
+		duckdb_state append() const
+		{
+			return duckdb_append_null(app);
+		}
+	};
+
 	struct string : public duckdb_string {
 		string(duckdb_string str)
 		{
@@ -21,6 +164,21 @@ namespace fms::duckdb {
 		~string()
 		{
 			duckdb_free(this);
+		}
+	};
+
+	struct date : public duckdb_date {
+		date(duckdb_date d)
+		{
+			*static_cast<duckdb_date*>(this) = d;
+		}
+		date(duckdb_date_struct d)
+		{
+			*static_cast<duckdb_date*>(this) = duckdb_to_date(d);
+		}
+		duckdb_date_struct from_date() const
+		{
+			return duckdb_from_date(*this);
 		}
 	};
 
@@ -49,20 +207,24 @@ namespace fms::duckdb {
 			return &res;
 		}
 
-		const char* column_name(idx_t col)
+		duckdb_type column_type(idx_t col) const
+		{
+			return duckdb_column_type(&res, col);
+		}
+		const char* column_name(idx_t col) const
 		{
 			return duckdb_column_name(&res, col);
 		}
-		idx_t column_count()
+		idx_t column_count() const
 		{
 			return duckdb_column_count(&res);
 		}
-		idx_t row_count()
+		idx_t row_count() const
 		{
 			return duckdb_row_count(&res);
 		}
 
-// https://duckdb.org/docs/archive/0.9.2/api/c/api#value-functions
+		// https://duckdb.org/docs/archive/0.9.2/api/c/api#value-functions
 #define DUCKDB_VALUE_IMPL(a,b) a value_##b(idx_t row, idx_t col) { return duckdb_value_##b(&res, row, col); }
 #define DUCKDB_VALUE(X) \
 	X(bool, boolean) \
@@ -81,55 +243,55 @@ namespace fms::duckdb {
 	X(char*, varchar) \
 	X(duckdb_blob, blob) \
 	X(string, string) \
+	X(date, date) \
 
 #if 0
-	X(string, date) \
-	X(string, time) \
-	X(string, timestamp) \
-	X(string, interval) \
-	X(string, hugeint) \
-	X(string, pointer) \
-	X(string, fixedsizebinary) \
-	X(string, list) \
-	X(string, map) \
-	X(string, struct) \
-	X(string, union) \
-	X(string, json) \
-	X(string, intervaldaytime) \
-	X(string, intervalyearmonth) \
-	X(string, time32) \
-	X(string, time64) \
-	X(string, timestamp_seconds) \
-	X(string, timestamp_milliseconds) \
-	X(string, timestamp_microseconds) \
-	X(string, timestamp_nanoseconds) \
-	X(string, timestamp_zoned_seconds) \
-	X(string, timestamp_zoned_milliseconds) \
-	X(string, timestamp_zoned_microseconds) \
-	X(string, timestamp_zoned_nanoseconds) \
-	X(string, date_zoned) \
-	X(string, time_zoned) \
-	X(string, interval_zoned) \
-	X(string, list_element) \
-	X(string, nested) \
-	X(string, dictionary) \
-	X(string, fixedsizelist) \
-	X(string, extension) \
-	X(string, chunk_collection) \
-	X(string, invalid) \
+		X(string, time) \
+			X(string, timestamp) \
+			X(string, interval) \
+			X(string, hugeint) \
+			X(string, pointer) \
+			X(string, fixedsizebinary) \
+			X(string, list) \
+			X(string, map) \
+			X(string, struct) \
+			X(string, union) \
+			X(string, json) \
+			X(string, intervaldaytime) \
+			X(string, intervalyearmonth) \
+			X(string, time32) \
+			X(string, time64) \
+			X(string, timestamp_seconds) \
+			X(string, timestamp_milliseconds) \
+			X(string, timestamp_microseconds) \
+			X(string, timestamp_nanoseconds) \
+			X(string, timestamp_zoned_seconds) \
+			X(string, timestamp_zoned_milliseconds) \
+			X(string, timestamp_zoned_microseconds) \
+			X(string, timestamp_zoned_nanoseconds) \
+			X(string, date_zoned) \
+			X(string, time_zoned) \
+			X(string, interval_zoned) \
+			X(string, list_element) \
+			X(string, nested) \
+			X(string, dictionary) \
+			X(string, fixedsizelist) \
+			X(string, extension) \
+			X(string, chunk_collection) \
+			X(string, invalid) \
 
 #endif // 0
 
-DUCKDB_VALUE(DUCKDB_VALUE_IMPL)
+			DUCKDB_VALUE(DUCKDB_VALUE_IMPL)
 #undef DUCKDB_VALUE_IMPL
 #undef DUCKDB_VALUE
-		
+
 	};
 
 	class database {
 		duckdb_database db;
 	public:
-		database(const char* path = nullptr) 
+		database(const char* path = nullptr)
 			: db{ nullptr }
 		{
 			duckdb_state state = duckdb_open(path, &db);
