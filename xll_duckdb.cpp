d65caf49595fc9ead09b76e235e0bf89f6a978a8
Keith A. Lewis <keithalewis@live.com>
2024-01-05 08:14:03 -0500

    handles

--- xll_duckdb.cpp
+++ xll_duckdb.cpp
@@ -1,23 +1,26 @@
 // xll_duckdb.cpp - DuckDB Excel Add-in
 #include "xll_duckdb.h"
 
+using namespace fms;
 using namespace xll;
 
 AddIn xai_duckdb(
-	Function(XLL_DOUBLE, L"xll_duckdb_open", CATEGORY L".OPEN")
+	Function(XLL_HANDLEX, L"xll_duckdb_open", CATEGORY L".OPEN")
 	.Arguments({
 		Arg(XLL_CSTRING4, L"path", L"is the path to a database."),
 	})
 	.Category(CATEGORY)
 	.FunctionHelp(L"Return the number of ducks.")
 );
-double WINAPI xll_duckdb_open(const char* path)
+HANDLEX WINAPI xll_duckdb_open(const char* path)
 {
 #pragma XLLEXPORT
-	double result = std::numeric_limits<double>::quiet_NaN();
+	double result = INVALID_HANDLEX;
 
 	try {
-		fms::duckdb::database db(path);
+		handle<duckdb::database> h(new duckdb::database(path));
+		ensure(h);
+		result = h.get();
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
